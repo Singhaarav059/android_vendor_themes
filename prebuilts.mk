b08296160e49a0349b6c97a873ba66b76dbcfb4e
Anushek Prasal <anushekprasal@gmail.com>
2019-09-28 01:14:50 +0530

    vendor: Add Gallery Go

--- prebuilts.mk
+++ prebuilts.mk
@@ -3,7 +3,8 @@ PRODUCT_PACKAGES += \
     NexusLauncherRelease \
     SettingsIntelligenceGooglePrebuilt \
     PixelThemes \
-    SoundPickerPrebuilt
+    SoundPickerPrebuilt \
+    GalleryGoPrebuilt
 
 # Overlays
 PRODUCT_PACKAGES += \
