bafe5194e36afa69aaf6e053ff61930788903af9
harsh-23 <harsh.v23labs@gmail.com>
2019-10-16 01:09:47 +0700

    accents: Add new accent colors for Q

--- common.mk
+++ common.mk
@@ -5,6 +5,9 @@ DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
 # Boot animation
 include vendor/themes/bootanimation.mk
 
+# Accent
+include vendor/themes/accent.mk
+
 # Set Pixel blue light theme on Gboard
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.ime.theme_id=5
