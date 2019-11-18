ec8c0e1a67e959a9bd2e1e95ec23d75e4784bdcb
Martincz Gao <martincz@mokeedev.com>
2019-11-19 05:12:56 +0800

    prebuilts: Improve Lawnchair build config
    Change-Id: Idb2030d16b9f5bae42abc41f012e0e08d5778570
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- app/Android.mk
+++ app/Android.mk
@@ -80,6 +80,18 @@ LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_SRC_FILES := $(LOCAL_MODULE)/$(LOCAL_MODULE).apk
 LOCAL_REPLACE_PREBUILT_APK_INSTALLED := $(LOCAL_PATH)/$(LOCAL_SRC_FILES)
+
+LOCAL_PREBUILT_JNI_LIBS_arm := \
+    Lawnchair/lib/armeabi-v7a/libhoko_blur.so
+
+LOCAL_PREBUILT_JNI_LIBS_arm64 := \
+    Lawnchair/lib/arm64-v8a/libhoko_blur.so
+
+LOCAL_PREBUILT_JNI_LIBS_x86 := \
+    Lawnchair/lib/x86/libhoko_blur.so
+
+LOCAL_PREBUILT_JNI_LIBS_x86_64 := \
+    Lawnchair/lib/x86_64/libhoko_blur.so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
